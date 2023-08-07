41272cc4d5b9811cad5f14654d218ac8a7384ea7
MANJUNATH SK <50445995+SK1793@users.noreply.github.com>
2023-08-07 21:38:35 +0530

    Update send_mail.php

--- portfolio/send_mail.php
+++ portfolio/send_mail.php
@@ -1,5 +1,5 @@
 <?php
-  $name = $_POST['name'];
+  /*$name = $_POST['name'];
   $email = $_POST['email'];
   $message = $_POST['message'];
   $to = 'manjusk017@gmail.com';
@@ -8,6 +8,7 @@
              'Subject:' . $subject . "\r\n".
              'Reply-To: ' . $email . "\r\n" .
               "CC:manjusk017@gmail.com";
+*/
     
 if ($_SERVER["REQUEST_METHOD"] === "POST" ){
 
@@ -30,7 +31,7 @@ $mailid->isHTML(true);
 
 $mailid->Subject="Thank you for visiting my website";
 $mailid->Body=$_POST['user_name'].  ", Thank you for visiting my website, I received your message,
-<br> Here is your message: " . $_POST['user_message'];
+<br> Here is your message: ". $_POST['user_message'];
 
 $mailid->send();
 
@@ -40,15 +41,15 @@ document.location.href='home.php';
     }
 
     catch(Exception $e){
-        echo "error: " .$mailid->ErrorInfo;
+        echo "<script>alert('Error: '.$mailid->ErrorInfo );</script> " ;
     }
 
 }else{
-    echo("Given Email is not correct!");
+    echo "<script>alert('Given Email is not correct!');</script>";
 }
 }
 else{
-    echo "<script>alert('Failed to load your request for some reason try again')</script>";
+    echo "<script>alert('Failed to load your request for some reason try again');</script>";
 }
   
 ?>
