0e6e6ae1a5ae12044e3662f3b7fac6d8b9bcd61e
MANJUNATH SK <50445995+SK1793@users.noreply.github.com>
2023-08-08 20:12:57 +0530

    Update send_mail.php

--- portfolio/send_mail.php
+++ portfolio/send_mail.php
@@ -25,9 +25,9 @@ $mailid->isSMTP();
 $mailid->Host='smtp.mailgun.org';
 $mailid->SMTPAuth=true;
 $mailid->Username='postmaster@sandbox5e814278a0274da6a8faf3a2e2a67666.mailgun.org';
-$mailid->Password='xefrwitagjpuklpy';
+$mailid->Password='63fc3cb306b475e86c9cc80df7bce81f-28e9457d-1e202ab5';
 $mailid->SMTPSecure='tls';
-$mailid->Port=465;
+$mailid->Port=587;
 
 $mailid->setFrom('manjusk017@gmail.com','Manjunath SK');
 
