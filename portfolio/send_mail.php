038ed87868f0ecc105d18ed41d572572347a7612
MANJUNATH SK <50445995+SK1793@users.noreply.github.com>
2023-08-08 15:07:55 +0530

    Add files via upload

--- portfolio/send_mail.php
+++ portfolio/send_mail.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -7,57 +7,56 @@ require 'assets/PHPMailer/PHPMailer/src/Exception.php';
 require 'assets/PHPMailer/PHPMailer/src/PHPMailer.php';
 require 'assets/PHPMailer/PHPMailer/src/SMTP.php';
 
-  /*$name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-  $to = 'manjusk017@gmail.com';
-  $subject = 'Message from ' . $name;
-  $headers = 'From: ' . $email . "\r\n" .
-             'Subject:' . $subject . "\r\n".
-             'Reply-To: ' . $email . "\r\n" .
-              "CC:manjusk017@gmail.com";
-*/
-    
+ 
+
+
 if ($_SERVER["REQUEST_METHOD"] === "POST" ){
 
-try{
-  
+
+    //Email validation and sending mail
+
+    if(filter_var($_POST['user_mail'],FILTER_VALIDATE_EMAIL)){
+
+    
+    try{
+
 $mailid=new PHPMailer(true);
 $mailid->isSMTP();
 $mailid->Host='smtp.gmail.com';
 $mailid->SMTPAuth=true;
 $mailid->Username='manjusk017@gmail.com';
-$mailid->Password='jsxfxhrowoonpotl';
+$mailid->Password='xefrwitagjpuklpy';
 $mailid->SMTPSecure=PHPMailer::ENCRYPTION_SMTPS;
 $mailid->Port=465;
 
 $mailid->setFrom('manjusk017@gmail.com','Manjunath SK');
 
 $mailid->addAddress($_POST["user_mail"],$_POST['user_name']);
-  
 $mailid->isHTML(true);
 
-$mailid->Subject="Thank you for visiting my website";
-$mailid->Body=$_POST['user_name'].  ", Thank you for visiting my website, I received your message,
-<br> Here is your message: ". $_POST['user_message'];
+$mailid->Subject=ucwords($_POST['user_name']) .' Thank you for Visiting My website';
+$mailid->Body=ucfirst($_POST['user_name']).  ",Thank you for visiting My website,i got your message,<br>If there's a need i will respond back... 
+<br> here's your message: " ."'". $_POST['user_message']."'"."<br><br>Have a Nice Day!<br><br> &nbsp;&nbsp; -Manjunath SK.";
 
 $mailid->send();
 
-echo "<script> alert('Successfull!');
-document.location.href='home.php';  
+echo "<script> alert('Message sent! we will send you a confirmation mail.');
+        document.location.href='index.html';
       </script>";
     }
 
     catch(Exception $e){
-        echo "<script>alert('Error: '.$mailid->ErrorInfo );</script> " ;
+        echo "error: " .$mailid->ErrorInfo;
     }
 
 }else{
-    echo "<script>alert('Given Email is not correct!');</script>";
+    echo("Given Email is not correct!");
 }
 }
 else{
-    echo "<script>alert('Failed to load your request for some reason try again');</script>";
+    echo "<script>alert('failed to load POST')</script>";
 }
-  
+
+
+
 ?>
